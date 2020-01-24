dd07b25ad123a8c6132601a2c2c1162a54eddf69
Lily Scott <me@suchipi.com>
2020-01-23 18:07:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# @hex-engine/2d game
+
+This repo was created using `create-hex-engine-game`.
+
+## Usage
+
+To run a development webserver for your game locally:
+
+```sh
+npm start
+```
+
+To build a production version of your game for redistribution:
+
+```sh
+npm run build
+```
+
+For more info, head on over to [Hex Engine's website](https://hex-engine.dev).
---
+++ src/Draggable.ts
@@ -0,0 +1,43 @@
+import {
+  useType,
+  useNewComponent,
+  useEntity,
+  Geometry,
+  Physics,
+  Mouse,
+  Point,
+} from "@hex-engine/2d";
+
+export default function Draggable(geometry: ReturnType<typeof Geometry>) {
+  useType(Draggable);
+
+  const physics = useEntity().getComponent(Physics.Body);
+
+  const mouse = useNewComponent(Mouse);
+
+  let originalStatic = false;
+  let isDragging = false;
+  const startedDraggingAt = new Point(0, 0);
+
+  mouse.onDown((event) => {
+    if (physics) {
+      originalStatic = physics.body.isStatic;
+      physics.setStatic(true);
+    }
+    isDragging = true;
+    startedDraggingAt.mutateInto(event.pos);
+  });
+
+  mouse.onMove((event) => {
+    if (isDragging) {
+      geometry.position.addMutate(event.pos.subtract(startedDraggingAt));
+    }
+  });
+
+  mouse.onUp(() => {
+    if (physics) {
+      physics.setStatic(originalStatic);
+    }
+    isDragging = false;
+  });
+}
